ab23ab283885dc7cf3fc2bee6b5122c97775cd6d
Sakhir <40635516+Sakhir-BTC@users.noreply.github.com>
2022-01-30 14:04:08 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+//
+// Created by Haifa Bogdan Adnan on 04/08/2018.
+//
+
+#ifndef IXIMINER_CONFIG_H_IN_H
+#define IXIMINER_CONFIG_H_IN_H
+
+#define IxiMiner_VERSION_MAJOR "0"
+#define IxiMiner_VERSION_MINOR "2"
+#define IxiMiner_VERSION_REVISION "0"
+
+#endif //IXIMINER_CONFIG_H_IN_H
